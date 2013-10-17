787c4155f8f4c3d2ac2af30b83d91ab43946dcdd
Daniel X Moore <yahivin@gmail.com>
2013-10-17 15:44:07 -0700

    :coffee::five: Updated in browser at strd6.github.io/editor

--- master.js
+++ master.js
@@ -16,6 +16,9 @@
         runningWindows.push(sandbox);
         return sandbox;
       },
+      testsHtml: function(testScripts) {
+        return "<html>\n<head>\n  <meta charset=\"utf-8\">\n  <title>Mocha Tests</title>\n  <link rel=\"stylesheet\" href=\"http://strd6.github.io/tests/mocha.css\"/>\n</head>\n<body>\n  <div id=\"mocha\"></div>\n  <script src=\"http://strd6.github.io/tests/assert.js\"><\/script>\n  <script src=\"http://strd6.github.io/tests/mocha.js\"><\/script>\n  <script>mocha.setup('bdd')<\/script>\n  " + testScripts + "\n  <script>\n    mocha.checkLeaks();\n    mocha.globals(['jQuery']);\n    mocha.run();\n  <\/script>\n</body>\n</html>";
+      },
       hotReloadCSS: function(css) {
         return runningWindows = runningWindows.select(function(window) {
           if (window.closed) {
